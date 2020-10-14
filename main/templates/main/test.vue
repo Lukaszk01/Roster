290f0d7c694651bb4a29f1896685f289cd45d8bd
Lukasz <lukaszkkoziel@gmail.com>
2020-10-14 19:36:58 +0900

    vue file

--- main/templates/main/test.vue
+++ main/templates/main/test.vue
@@ -5,12 +5,15 @@
     <div>[[ message ]]</div>
 </div>
 {{ username|json_script:"username" }}
-
+<div class="calculator">
+<div class="display">{{ current }}</div>
+</div?
 <script>
 var username = JSON.parse(document.getElementById('username').textContent);
 var app = new Vue({
   data() {
       return {
+        current: '',
         items: [
           { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
           { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
@@ -26,4 +29,19 @@ var app = new Vue({
   },
 });
 </script>
+<style scoped>
+.display {
+  grid-column: 1 / 5;
+  background-color: #333;
+  color: white;
+}
+.calculator {
+  margin: 0 auto;
+  width: 400px;
+  font-size: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
+</style>
 {% endblock %}
