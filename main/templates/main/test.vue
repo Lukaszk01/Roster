68ca27b6e6423ab1f476c6bb55fd04c78f333864
Lukasz <lukaszkkoziel@gmail.com>
2020-10-15 18:29:00 +0900

    info templet

--- main/templates/main/test.vue
+++ main/templates/main/test.vue
@@ -5,15 +5,49 @@
     <div>[[ message ]]</div>
 </div>
 {{ username|json_script:"username" }}
-<div class="calculator">
-<div class="display">{{ current }}</div>
-</div>
+  <div class="home-view-container">
+    <h1>Add your info</h1>
+      <h3>
+        <font-awesome-icon icon="cat"/>
+        {{ getAllCats.length }} +
+        <font-awesome-icon icon="dog"/>
+        {{ getAllDogs.length }}
+      </h3>
+    <button @click="togglePetForm" class="btn btn-primary">Add New Pet</button>
+
+    <b-form @submit.prevent="handleSubmit" v-if="showPetForm">
+      <b-form-group id="exampleInputGroup2" label="Pet's Name:" label-for="exampleInput2">
+        <b-form-input
+          id="exampleInput2"
+          type="text"
+          v-model="formData.name"
+          required
+          placeholder="Enter name" />
+      </b-form-group>
+
+      <b-form-group id="exampleInputGroup3" label="Species:" label-for="exampleInput3">
+        <b-form-select id="exampleInput3" :options="['cats', 'dogs']" required v-model="formData.species" />
+      </b-form-group>
+
+      <b-form-group id="exampleInputGroup2" label="Pet's Age:" label-for="exampleInput2">
+        <b-form-input
+          id="exampleInput2"
+          type="number"
+          v-model="formData.age"
+          required
+          placeholder="Enter age" />
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+  </div>
 <script>
 var username = JSON.parse(document.getElementById('username').textContent);
 var app = new Vue({
   data() {
       return {
-        current: '',
+
         items: [
           { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
           { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
@@ -22,34 +56,17 @@ var app = new Vue({
         ]
       }
     },
-    methods: {
-    clear() {
-      this.current = '';
-    },
-    sign() {
-      this.current = this.current.charAt(0) === '-' ?
-      this.current.slice(1) : `-${this.current}`;
-    }
+  
+
   delimiters: ["[[", "]]"],
   el: '#app',
   data: {
     message: 'Elo elo 320 vue is working on this app mdf! ' + username
   },
 });
+
 </script>
 <style scoped>
-.display {
-  grid-column: 1 / 5;
-  background-color: #333;
-  color: white;
-}
-.calculator {
-  margin: 0 auto;
-  width: 400px;
-  font-size: 40px;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-auto-rows: minmax(50px, auto);
-}
+
 </style>
 {% endblock %}
