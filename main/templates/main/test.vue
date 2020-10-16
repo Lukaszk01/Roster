84b8d1167c4520a7745d85dd8d471fa2f0fc8c88
Lukasz <lukaszkkoziel@gmail.com>
2020-10-17 07:54:11 +0900

    list

--- main/templates/main/test.vue
+++ main/templates/main/test.vue
@@ -1,76 +1,81 @@
 {% extends "main/base.html" %}
-
 {% block content %}
-  <div class="mt-4" id="app">
-    <div>[[ message ]]</div>
+
+<div id="staggered-list-demo">
+  <input v-model="query">
+  <transition-group
+    name="staggered-fade"
+    tag="ul"
+    v-bind:css="false"
+    v-on:before-enter="beforeEnter"
+    v-on:enter="enter"
+    v-on:leave="leave"
+  >
+    <li
+      v-for="(item, index) in computedList"
+      v-bind:key="item.msg"
+      v-bind:data-index="index"
+    >{{ item.msg }}</li>
+  </transition-group>
 </div>
-{{ username|json_script:"username" }}
-  <div class="home-view-container">
-    <h1>Add your info</h1>
-      <h3>
-        <font-awesome-icon icon="cat"/>
-        {{ getAllCats.length }} +
-        <font-awesome-icon icon="dog"/>
-        {{ getAllDogs.length }}
-      </h3>
-    <button @click="togglePetForm" class="btn btn-primary">Add New Pet</button>
 
-    <b-form @submit.prevent="handleSubmit" v-if="showForm">
-      <b-form-group id="exampleInputGroup2" label="Pet's Name:" label-for="exampleInput2">
-        <b-form-input
-          id="exampleInput2"
-          type="text"
-          v-model="formData.name"
-          required
-          placeholder="Enter name" />
-      </b-form-group>
-
-      <b-form-group id="exampleInputGroup3" label="Species:" label-for="exampleInput3">
-        <b-form-select id="exampleInput3" :options="['cats', 'dogs']" required v-model="formData.species" />
-      </b-form-group>
-
-      <b-form-group id="exampleInputGroup2" label="Pet's Age:" label-for="exampleInput2">
-        <b-form-input
-          id="exampleInput2"
-          type="number"
-          v-model="formData.age"
-          required
-          placeholder="Enter age" />
-      </b-form-group>
-
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
-  </div>
 <script>
-var username = JSON.parse(document.getElementById('username').textContent);
-var app = new Vue({
-  data() {
-      return {
-        showForm: false,
-        formData: {
-        name: '',
-        age: 0,
-        species: null
-      },
-      
-
-  delimiters: ["[[", "]]"],
-  el: '#app',
+new Vue({
+  el: '#staggered-list-demo',
   data: {
-    message: 'Elo elo 320 vue is working on this app mdf! ' + username
+    query: '',
+    list: [
+      { msg: 'Bruce Lee' },
+      { msg: 'Jackie Chan' },
+      { msg: 'Chuck Norris' },
+      { msg: 'Jet Li' },
+      { msg: 'Jet Li' },
+      { msg: 'Jet Li' },
+      { msg: 'Jet Li' },
+      { msg: 'Jet Li' },
+      { msg: 'Kung Fury' }
+    ]
+  },
+  computed: {
+    computedList: function () {
+      var vm = this
+      return this.list.filter(function (item) {
+        return item.msg.toLowerCase().indexOf(vm.query.toLowerCase()) !== -1
+      })
+    }
   },
   methods: {
-    ...mapActions([
-      'addPet'
-    ]),
-    togglePetForm () {
-      this.showForm = !this.showForm
+    beforeEnter: function (el) {
+      el.style.opacity = 0
+      el.style.height = 0
     },
-};
-
+    enter: function (el, done) {
+      var delay = el.dataset.index * 150
+      setTimeout(function () {
+        Velocity(
+          el,
+          { opacity: 1, height: '1.6em' },
+          { complete: done }
+        )
+      }, delay)
+    },
+    leave: function (el, done) {
+      var delay = el.dataset.index * 150
+      setTimeout(function () {
+        Velocity(
+          el,
+          { opacity: 0, height: 0 },
+          { complete: done }
+        )
+      }, delay)
+    }
+  }
+})
 </script>
 <style scoped>
+  #staggered-list-demo {
+    color: pink;
 
+}
 </style>
 {% endblock %}
