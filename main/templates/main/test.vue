5cc1264f9097599073e625822250bbb3c277da66
Lukasz <lukaszkkoziel@gmail.com>
2020-11-29 07:13:32 +0900

    test

--- main/templates/main/test.vue
+++ main/templates/main/test.vue
@@ -7,15 +7,6 @@
   <button-counter></button-counter>
 </div>
 <script>
-export default {
-  Vue.component('button-counter', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
-})
-}
+
 </script>
 {% endblock content %}
