64715a07fe530a87a262a3802322e8934ab63cb5
Lukasz <lukaszkkoziel@gmail.com>
2020-12-02 15:52:41 +0900

    test is back

---
+++ main/templates/main/test.vue
@@ -0,0 +1,21 @@
+{% extends "main/base.html" %}
+{% block content %}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.min.js"></script>
+
+
+<div id="components-demo">
+  <button-counter></button-counter>
+</div>
+<script>
+export default {
+  Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+}
+</script>
+{{% endblock content %}}
