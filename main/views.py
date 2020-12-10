b4bcccb65225c1ffcc5e7392fb4a479be3c4ce85
Lukasz <lukaszkkoziel@gmail.com>
2020-12-10 19:04:16 +0900

    forms

--- main/views.py
+++ main/views.py
@@ -22,21 +22,28 @@ def management(request):
 def notifications(request):
     return render(request, 'main/notifications.html', {'title': 'notifications'})
 
-def get_name(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
+def post(self, request):
+        form = HomeForm(request.POST)
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
 
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
+            text = form.cleaned_data['post']
+            form = HomeForm()
+            return redirect('home:home')
 
-    return render(request, 'name.html', {'form': form})
+        args = {'form': form, 'text': text}
+        return render(request, self.template_name, args)
 
+def get(self, request):
+        form = HomeForm()
+        posts = Post.objects.all().order_by('-created')
+        users = User.objects.exclude(id=request.user.id)
+        friend = Friend.objects.get(current_user=request.user)
+        friends = friend.users.all()
+
+        args = {
+            'form': form, 'posts': posts, 'users': users, 'friends': friends
+        }
+        return render(request, self.template_name, args)
--- user/forms.py
+++ user/forms.py
@@ -64,7 +64,7 @@ class UserUpdateForm(forms.ModelForm):
       fields = ['username', 'email', 'gender', 'full_name']
 
 
-class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
+class HomeForm(forms.Form):
+    post = forms.CharField()
 
 
