32ac41db54f2a236308ebf8181d21daeffd4a6fb
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-03-10 11:23:10 +0900

    component created

--- emploee-form/src/components/BaseRadioGroup.vue
+++ emploee-form/src/components/BaseRadioGroup.vue
@@ -1,12 +1,47 @@
 <template>
-    
+  <component
+    v-for="option in options"
+    :key="option.value"
+    :is="vertical ? 'div' : 'span'"
+    :class="{
+      'horizontal': !vertical
+    }"
+  >
+    <BaseRadio
+      :label="option.label"
+      :value="option.value"
+      :modelValue="modelValue"
+      :name="name"
+      @update:modelValue="$emit('update:modelValue', $event)"
+    />
+  </component>
 </template>
 
 <script>
 export default {
-const radioOptions = [
-  { label: 'Gud boi', value: 'dog' },
-  { label: 'Angri boi', value: 'cat' }
-]
+  props: {
+    options: {
+      type: Array,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    modelValue: {
+      type: [String, Number],
+      required: true
+    },
+    vertical: {
+      type: Boolean,
+      default: false
+    }
+  }
 }
 </script>
+
+<style scoped>
+.horizontal {
+  margin-right: 20px;
+}
+</style>
