16bc8e47f0cb7520691f18b52ae2080a92a056da
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-05-21 12:21:31 +0900

    odels issue

--- user/models.py
+++ user/models.py
@@ -23,7 +23,7 @@ class Customer(models.Model):
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=120)
+    title       = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(decimal_places=2, max_digits=10000)
+    price       = models.DecimalField(decimal_places=2, max_digits=10000)
     
