4cc839c3f6b211bfb1a32852dcf06fec5140199f
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-09-12 16:05:37 +0900

    building simple database(unrelated data)

--- Roster_CLI/staff/main_menu.py
+++ Roster_CLI/staff/main_menu.py
@@ -4,16 +4,15 @@ import json
 class Staff_menu():
     print ("Enter your option: \n 1: Personal details \n 2: Set your shifts \n 3: Upcoming shifts \n"
     )
-    option = int(input("enter your option. from 1-3 to get the name on month : "))
+    option = int(input("enter your option. from 1-3: "))
 
     if option == 1:
         for d in Personal_details.details.items():
             print (d)
         # print (details.email)
  
-    elif option == 2:
-        result =  "febuary"
-        print("the month is ", result)
+    elif option == 'q':
+        print("Thank you for using the app ")
  
     elif option == 3:
         result = "march"
